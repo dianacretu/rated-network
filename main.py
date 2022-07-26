bccc788cf50d058ce427e65a11cb9fc0702b43eb
Diana Cretu <dianacretu2806@gmail.com>
2022-07-26 12:50:30 -0700

    Compute gas cost in gwei for each transaction

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
 import csv
 from datetime import datetime
-from transaction import transaction 
+from transaction import transaction
 import transaction_timestamp
 import sys
+import transaction_gas_cost
 
 args = sys.argv[1:]
 
@@ -27,6 +28,10 @@ with open(file, 'r') as csvf:
 latest_block_timestamp = "26.07.2022 12:32:54 AM UTC"
 latest_block_number = 15218113
 
-# Calculate the timestamp for each transaction
+# calculate the timestamp for each transaction
 for key in data:
     print(transaction_timestamp.find_transaction_timestamp(key, data, latest_block_timestamp, latest_block_number))
+
+# calculate the gas cost in gwei for each transaction
+for key in data:
+    print(transaction_gas_cost.find_transaction_gas_cost(key, data))
